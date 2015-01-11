98e76a1bf7f087a3b92d5d8db8ff1e7c3c5710c3
Myrcheane Contridas <myrcheane@bootyard.com>
2015-01-11 11:50:40 +0800

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :pints
 
   devise_for :users
-  root "pints#index"
+  root :to => 'pints#index'
   get "about" =>"pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
